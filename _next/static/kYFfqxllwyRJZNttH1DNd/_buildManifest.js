1f6b5561d657c311da067bda5ab71134c4c08385
agha-0 <ahmedtayyab533@gmail.com>
2026-06-15 12:48:02 +0500

    Updates

---
+++ _next/static/kYFfqxllwyRJZNttH1DNd/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/spotgamma-frontend-takehome//_next/:path+",
+        "destination": "/spotgamma-frontend-takehome/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
